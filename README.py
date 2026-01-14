b10a7df0b1b5ff11b73babbde1db59472a8c425a
Sinchana2906 <psinchanaBTECH24@ced.alliance.edu.in>
2026-01-14 11:52:02 +0530

    Create README.py

---
+++ README.py
@@ -0,0 +1,13 @@
+# 8 Queens with Adjacency Constraint
+
+## Problem
+Place 8 queens on a chessboard such that:
+- No two queens attack each other.
+- No two queens are placed next to each other.
+
+## Solution
+Implemented using backtracking in Python.
+
+## Run
+```bash
+python queens.py
